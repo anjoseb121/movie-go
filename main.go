8016adca631a2239bd510b3fc4cbcdc5f59be737
Antonio Barandica <anjose.121@gmail.com>
2018-09-09 18:50:11 -0500

    Use APIGateway events response and request Refactor tests

--- main.go
+++ main.go
@@ -8,16 +8,20 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 var (
-	ErrorBackend = errors.New("Somenthing went wrong")
-	ErrorAPIKey  = errors.New("API Key not found")
+	ErrorAPIKey        = errors.New("API Key not found")
+	ErrorMakingRequest = errors.New("Error creating new request")
+	ErrorBody          = errors.New("Error parsing clients request params")
+	ErrorAPIReq        = errors.New("Error making movie API request")
+	ErrorMovieResponse = errors.New("Error parsing movie API response")
 )
 
-// Request from client
-type Request struct {
+// ClientBody from client
+type ClientBody struct {
 	ID int `json:"id"`
 }
 
@@ -34,40 +38,68 @@ type Movie struct {
 	ReleaseDate string `json:"release_date"`
 }
 
+func responseError(err error) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: 500,
+		Body:       err.Error(),
+	}, err
+}
+
 // Handler function, do the Lambda exec logic
-func Handler(request Request) ([]Movie, error) {
-	APIKey, ok := os.LookupEnv("API_KEY")
-	if !ok {
-		return []Movie{}, ErrorAPIKey
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	APIKey, APIKeyFound := os.LookupEnv("API_KEY")
+	if !APIKeyFound {
+		return responseError(ErrorAPIKey)
 	}
 
+	// Init
 	movieURL := fmt.Sprintf("https://api.themoviedb.org/3/discover/movie?api_key=%s", APIKey)
-
 	client := &http.Client{}
 
 	req, err := http.NewRequest("GET", movieURL, nil)
 	if err != nil {
-		return []Movie{}, ErrorBackend
+		return responseError(ErrorMakingRequest)
 	}
 
-	if request.ID > 0 {
-		q := req.URL.Query()
-		q.Add("with_genres", strconv.Itoa(request.ID))
-		req.URL.RawQuery = q.Encode()
+	if request.Body != "" {
+		// Parse client request body to struct
+		var body ClientBody
+		err = json.Unmarshal([]byte(request.Body), &body)
+		if err != nil {
+			return responseError(ErrorBody)
+		}
+
+		// If request id param is greater than 0 add genres
+		if body.ID > 0 {
+			q := req.URL.Query()
+			q.Add("with_genres", strconv.Itoa(body.ID))
+			req.URL.RawQuery = q.Encode()
+		}
 	}
 
+	// Do MovieAPI request
 	resp, err := client.Do(req)
 	if err != nil {
-		return []Movie{}, ErrorBackend
+		return responseError(ErrorAPIReq)
 	}
 	defer resp.Body.Close()
 
+	// Parse movieAPI response to struct
 	var data MovieDBResponse
 	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
-		return []Movie{}, ErrorBackend
+		return responseError(ErrorMovieResponse)
 	}
 
-	return data.Movies, nil
+	// Parse movie response to json
+	movies, err := json.Marshal(data.Movies)
+	if err != nil {
+		return responseError(ErrorMovieResponse)
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       string(movies),
+	}, nil
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -4,14 +4,29 @@ import (
 	"os"
 	"testing"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestHandler(t *testing.T) {
 	os.Setenv("API_KEY", "MockingAPIKey")
-	movies, err := Handler(Request{
-		ID: 18,
-	})
-	assert.IsType(t, nil, err)
-	assert.NotEqual(t, 0, len(movies))
+	tests := []struct {
+		request events.APIGatewayProxyRequest
+		err     error
+	}{
+		{
+			// Test that the handler responds with the correct body
+			request: events.APIGatewayProxyRequest{Body: `{"id": 18}`},
+			err:     nil,
+		},
+		{
+			// Test that the handler responds with the empty body
+			request: events.APIGatewayProxyRequest{Body: ``},
+			err:     nil,
+		},
+	}
+	for _, test := range tests {
+		_, err := Handler(test.request)
+		assert.IsType(t, test.err, err)
+	}
 }
